d350cb176316d6859f56add55dedbbdfe5b18dce
spehil <boo1231@naver.com>
2023-09-25 11:06:44 +0900

    #2 hello world 출력 추가 정말 기가 막히게 hello world를 추가할수 있는 기능을 개발하였습니다.

--- src/main/java/com/example/sdsada/SdsadaApplication.java
+++ src/main/java/com/example/sdsada/SdsadaApplication.java
@@ -8,6 +8,7 @@ public class SdsadaApplication {
 
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hi");
         SpringApplication.run(SdsadaApplication.class, args);
     }
 
