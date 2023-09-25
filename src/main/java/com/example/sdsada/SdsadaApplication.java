b9d95f623b9ba77124cc81af08c11455e17471ae
spehil <boo1231@naver.com>
2023-09-25 11:03:02 +0900

    git init

--- src/main/java/com/example/sdsada/SdsadaApplication.java
+++ src/main/java/com/example/sdsada/SdsadaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SdsadaApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello");
         SpringApplication.run(SdsadaApplication.class, args);
     }
 
