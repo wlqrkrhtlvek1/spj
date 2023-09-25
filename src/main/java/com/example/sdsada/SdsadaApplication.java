12d249612d961d2568454d69e6440e040fe89dec
spehil <boo1231@naver.com>
2023-09-25 11:01:37 +0900

    git init

---
+++ src/main/java/com/example/sdsada/SdsadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdsada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdsadaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sdsada/SdsadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdsada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
